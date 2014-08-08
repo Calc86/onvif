db1bc498659d39cee411d1e5f7f687a26784d17e
calc <calc@list.ru>
2014-08-08 15:52:05 +0400

    Доп классы для авторизации onvif

--- src/com/onvif/JAXBContext.java
+++ src/com/onvif/JAXBContext.java
@@ -1,7 +1,12 @@
 package com.onvif;
 
+import org.oasis_open.docs.wss._2004._01.oasis_200401_wss_wssecurity_secext_1_0.SecurityHeaderType;
+import org.oasis_open.docs.wss._2004._01.oasis_200401_wss_wssecurity_secext_1_0.UsernameTokenType;
+import org.oasis_open.docs.wss._2004._01.oasis_200401_wss_wssecurity_utility_1_0.AttributedDateTime;
 import org.onvif.ver10.device.wsdl.GetCapabilities;
 import org.onvif.ver10.device.wsdl.GetCapabilitiesResponse;
+import org.onvif.ver10.device.wsdl.GetSystemDateAndTime;
+import org.onvif.ver10.device.wsdl.GetSystemDateAndTimeResponse;
 import org.onvif.ver10.media.wsdl.*;
 import org.w3._2003._05.soap_envelope.Body;
 import org.w3._2003._05.soap_envelope.Envelope;
@@ -35,6 +40,13 @@ public class JAXBContext {
         classes.add(GetStreamUriResponse.class);
         classes.add(GetSnapshotUri.class);
         classes.add(GetSnapshotUriResponse.class);
+
+        classes.add(GetSystemDateAndTime.class);
+        classes.add(GetSystemDateAndTimeResponse.class);
+
+        classes.add(SecurityHeaderType.class);
+        classes.add(UsernameTokenType.class);
+        classes.add(AttributedDateTime.class);
 
         instance = javax.xml.bind.JAXBContext.newInstance(classes.toArray(new Class[0]));
     }
