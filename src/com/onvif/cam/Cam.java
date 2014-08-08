5a4660e7fa9b5a118310bee8e8d8d1dbc355c0a1
calc <calc@list.ru>
2014-08-08 15:54:05 +0400

    Поддержка onvif авторизации через URL http://user:pass@host/blablabla

--- src/com/onvif/cam/Cam.java
+++ src/com/onvif/cam/Cam.java
@@ -1,18 +1,29 @@
 package com.onvif.cam;
 
+import com.onvif.JAXBContext;
 import com.onvif.net.HTTP;
 import com.onvif.Query;
 import com.onvif.Response;
+import com.onvif.security.Security;
 import org.onvif.ver10.device.wsdl.GetCapabilities;
 import org.onvif.ver10.device.wsdl.GetCapabilitiesResponse;
+import org.onvif.ver10.device.wsdl.GetSystemDateAndTime;
+import org.onvif.ver10.device.wsdl.GetSystemDateAndTimeResponse;
 import org.onvif.ver10.media.wsdl.*;
 import org.onvif.ver10.schema.CapabilityCategory;
-import org.onvif.ver10.schema.Profile;
+import org.onvif.ver10.schema.DateTime;
 import org.onvif.ver10.schema.StreamSetup;
-import org.onvif.ver10.schema.Transport;
-import org.onvif.ver10.schema.TransportProtocol;
 import org.onvif.ver10.schema.StreamType;
+import org.onvif.ver10.schema.Transport;
+import org.onvif.ver10.schema.Profile;
+import org.onvif.ver10.schema.TransportProtocol;
+import org.w3._2003._05.soap_envelope.Body;
+import org.w3._2003._05.soap_envelope.Envelope;
+import org.w3._2003._05.soap_envelope.Header;
+import sun.java2d.SurfaceDataProxy;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.namespace.QName;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -27,7 +38,14 @@ import java.util.List;
 public class Cam {
     public static final TransportProtocol DEFAULT_PROTOCOL = TransportProtocol.TCP;
     public static final StreamType DEFAULT_STREAM = StreamType.RTP_UNICAST;
-    private URL onvif;
+    private URL onvifURL;
+
+    private boolean authorize = false;
+    private String user;
+    private String password;
+    private Header header = null;
+
+
     private URL onvifDevice;
     private URL onvifMedia;
     private Query query = new Query();
@@ -45,14 +63,20 @@ public class Cam {
 
     private List<Profile> profiles;
 
-    public Cam(String onvif) throws MalformedURLException {
-        this(onvif, DEFAULT_PROTOCOL, DEFAULT_STREAM);
+    public Cam(String onvifURL) throws MalformedURLException {
+        this(onvifURL, DEFAULT_PROTOCOL, DEFAULT_STREAM);
     }
 
-    public Cam(String onvif, TransportProtocol protocol, StreamType stream) throws MalformedURLException {
+    public Cam(String onvifURL, TransportProtocol protocol, StreamType stream) throws MalformedURLException {
         transportProtocol = protocol;
         streamType = stream;
-        this.onvif = new URL(onvif);
+        this.onvifURL = new URL(onvifURL);
+        if(this.onvifURL.getUserInfo() != null){
+            authorize = true;
+            String[] tmp = this.onvifURL.getUserInfo().split(":");
+            user = tmp[0];
+            if(tmp.length>1) password = tmp[1];
+        }
     }
 
     public void setDebug(boolean value){
@@ -65,15 +89,24 @@ public class Cam {
 
     public void init() throws IOException {
         setEndpoints();
+        setHeader();
         setProfiles();
     }
 
     private String query(URL endPoint, Object o) throws IOException {
         if(endPoint == null) return "";
 
-        String query =this.query.getXML(o);
+        String query =this.query.getXML(o, header);
         if(isDebug) System.out.println(query);
-        String response = http.post(endPoint, query);
+        String response = null;
+        try {
+            response = http.post(endPoint, query);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println(endPoint);
+            System.err.println(query);
+            throw e;
+        }
         if(isDebug) System.out.println(response);
 
         if(isSave){
@@ -102,7 +135,7 @@ public class Cam {
         GetCapabilities getCapabilities = new GetCapabilities();
         getCapabilities.getCategory().add(CapabilityCategory.ALL);
 
-        String response = query(onvif, getCapabilities);
+        String response = query(onvifURL, getCapabilities);
 
         Response<GetCapabilitiesResponse> r = new Response<>();
         GetCapabilitiesResponse capabilities = r.getResponse(response);
@@ -132,6 +165,27 @@ public class Cam {
             onvifPTZ = new URL(capabilities.getCapabilities().getPTZ().getXAddr());
     }
 
+    private void setHeader() throws IOException {
+        if(!authorize) return;
+
+        GetSystemDateAndTimeResponse r = getSystemDateAndTime();
+        DateTime dateTime = r.getSystemDateAndTime().getLocalDateTime();
+
+        Security security = new Security(dateTime, user, password, "calcOnvif");
+        header = security.buildHeader();
+    }
+
+    public void test(Security security){
+        Header header = security.buildHeader();
+        /*Body body = new Body();
+        Envelope e = new Envelope();
+        e.setHeader(header);
+        e.setBody(body);*/
+
+        System.out.println(query.getXML(null, header));
+
+    }
+
     private void setProfiles() throws IOException {
         String response = query(onvifMedia, new GetProfiles());
 
@@ -139,6 +193,15 @@ public class Cam {
         GetProfilesResponse profilesResponse = r.getResponse(response);
 
         profiles = profilesResponse.getProfiles();
+    }
+
+    public GetSystemDateAndTimeResponse getSystemDateAndTime() throws IOException {
+        GetSystemDateAndTime getSystemDateAndTime = new GetSystemDateAndTime();
+
+        String response = query(onvifMedia, getSystemDateAndTime);
+
+        Response<GetSystemDateAndTimeResponse> r = new Response<>();
+        return r.getResponse(response);
     }
 
     public GetStreamUriResponse getStreamUri(Profile profile) throws IOException {
@@ -169,5 +232,34 @@ public class Cam {
 
     public List<Profile> getProfiles() {
         return profiles;
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            JAXBContext.create();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        GetSystemDateAndTimeResponse r = null;
+        try {
+            Cam cam = new Cam("http://admin:admin@10.112.28.231/onvif/device_service");
+            cam.init();
+            //r.getSystemDateAndTime().
+            //cam.init();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*try {
+            URL url = new URL("http://admin@10.112.28.231/onvif/device_service");
+            System.out.println(url.getAuthority());
+            System.out.println(url.getUserInfo());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }*/
     }
 }
